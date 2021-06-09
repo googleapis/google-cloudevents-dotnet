4a91cedafb71720c1123b3c54f648bab9f3da115
Jon Skeet <jonskeet@google.com>
2021-06-09 16:01:04 +0100

    Initial extension attributes

---
+++ src/Google.Events.Protobuf/Cloud/Audit/V1/ExtensionAttributes.g.cs
@@ -0,0 +1,32 @@
+// Copyright 2021, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using CloudNative.CloudEvents;
+
+namespace Google.Events.Protobuf.Cloud.Audit.V1
+{    
+    /// <summary>Extension attributes for Google.Events.Protobuf.Cloud.Audit.V1 events.</summary>
+    public static class ExtensionAttributes
+    {
+        /// <summary>The name of the service which triggered the Cloud Audit log entry.</summary>
+        public static CloudEventAttribute ServiceName { get; } = CloudEventAttribute.CreateExtension("servicename", CloudEventAttributeType.String);
+
+        /// <summary>The name of the method (RPC) triggering the Cloud Audit log entry.</summary>
+        public static CloudEventAttribute MethodName { get; } = CloudEventAttribute.CreateExtension("methodname", CloudEventAttributeType.String);
+
+        /// <summary>The name of the resource that is the subject of the Cloud Audit log entry.</summary>
+        public static CloudEventAttribute ResourceName { get; } = CloudEventAttribute.CreateExtension("resourcename", CloudEventAttributeType.String);
+
+    }
+}
